ce5ff2dff6f15174f0fffc82e60825bd347a47f4
vishrut1308 <115770400+vishrut1308@users.noreply.github.com>
2022-12-13 18:28:01 +0530

    commit message

---
+++ application/views/templates/footer.php
@@ -0,0 +1,16 @@
+
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 0.1.0
+    </div>
+    <strong>Copyright &copy; <?php echo $company_data['company_name'] ?? '' ?>  Satyam and Vishrut
+  </footer>
+
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+
+</body>
+</html>
